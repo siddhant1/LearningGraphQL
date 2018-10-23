0cf65fae6dd2c55c8755a899b03ec6c852dabdc4
siddhant1 <sidhant.manchanda@gmail.com>
2018-10-23 19:10:08 +0530

    Querying Practice and challenges completed

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -26,12 +26,52 @@ const posts = [
   {
     id: "123",
     title: "GAG",
-    body: "GAG"
+    body: "GAG",
+    author: "1"
   },
   {
-    id: "123",
+    id: "1234",
+    title: "GAGA",
+    body: "GAGA",
+    author: "1"
+  },
+  {
+    id: "1235",
     title: "Hey",
-    body: "hey"
+    body: "hey",
+    author: "2"
+  }
+];
+const users = [
+  {
+    id: "1",
+    name: "Siddhant",
+    age: 12
+  },
+  {
+    id: "2",
+    name: "Alok",
+    age: 11
+  }
+];
+const comments = [
+  {
+    id: "1",
+    text: "Hey i am Siddhant , this is a test comment",
+    author: "1",
+    post: "123"
+  },
+  {
+    id: "2",
+    text: "hey i am another siddhant and this is also a test comment",
+    author: "1",
+    post: "1234"
+  },
+  {
+    id: "3",
+    text: "This worked for me thanks",
+    author: "2",
+    post: "123"
   }
 ];
 
@@ -40,16 +80,32 @@ const posts = [
 const typeDefs = `
 type Query{
     post(query:String):[Post!]!
+    user:[User!]!
     greeting(name: String):String!
     add(a: Float!,b: Float!):Float!
     add1(arr:[Float!]!):Float!
     grades:[Int!]!
+    comment:[Comment!]!
 }
-
+type User {
+  id:ID!,
+  name:String!
+  age:Int
+  posts:[Post!]!
+  comments:[Comment!]!
+}
 type Post{
     id:ID!
     title:String
     body:String!
+    author:User!
+    comments:[Comment!]!
+}
+type Comment{
+  id:ID!
+  text:String!
+  author:User!
+  post:Post!
 }
 
 `;
@@ -87,6 +143,9 @@ const resolvers = {
       let newposts = posts.filter(e => e.title.includes(query));
       return newposts;
     },
+    user(parent, args, ctx, info) {
+      return users;
+    },
     greeting(parent, args, ctx, info) {
       return `Hello ${args.name || ""}`;
     },
@@ -104,6 +163,44 @@ const resolvers = {
         result += element;
       });
       return result;
+    },
+    comment(parent, args, ctx, info) {
+      return comments;
+    }
+  },
+  Post: {
+    author(parent, info, ctx, inf) {
+      // in this case parent is the individual Post on which the query is running
+      const { author } = parent;
+      return users.find(user => {
+        return user.id === author;
+      });
+    },
+    comments(parent, info, ctx, inf) {
+      const { id } = parent;
+      return comments.filter(comment => comment.post == id);
+    }
+  },
+  User: {
+    posts(parent, args, ctx, info) {
+      const { id } = parent;
+      return posts.filter(post => {
+        return post.author === id;
+      });
+    },
+    comments(parent, args, ctx, info) {
+      const { id } = parent;
+      return comments.filter(comment => comment.author == id);
+    }
+  },
+  Comment: {
+    author(parent, args, ctx, info) {
+      const { author } = parent;
+      return users.find(user => user.id == author);
+    },
+    post(parent, args, ctx, info) {
+      const { post } = parent;
+      return posts.find(pst => pst.id == post);
     }
   }
 };

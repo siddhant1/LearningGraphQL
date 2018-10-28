5dc6faa52645b30b3d6734d30d6d16a1d11c0b5a
siddhant1 <sidhant.manchanda@gmail.com>
2018-10-28 17:49:38 +0530

    Added Promises Examples

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -10,10 +10,43 @@ const prisma = new Prisma({
 //prisma.subscription
 //prisma.exists
 
-prisma.query.users(null, "{id name posts { id title}}").then(data => {
-  console.log(JSON.stringify(data, undefined, 4));
-});
+// prisma.query.users(null, "{id name posts { id title}}").then(data => {
+//   console.log(JSON.stringify(data, undefined, 4));
+// });
 
-prisma.query
-  .comments(null, "{id text author {id name}}")
-  .then(data => console.log(JSON.stringify(data, undefined, 4)));
+// prisma.query
+//   .comments(null, "{id text author {id name}}")
+//   .then(data => console.log(JSON.stringify(data, undefined, 4)));
+
+// prisma.mutation
+//   .createPost(
+//     {
+//       data: {
+//         title: "My new gql is livelol",
+//         body: "YOu can find the new one here",
+//         published: true,
+//         author: {
+//           connect: {
+//             id: "cjnqchvxl00380805jy99p37y"
+//           }
+//         }
+//       }
+//     },
+//     "{id title body published author {id name}}"
+//   )
+//   .then(data => {
+//     console.log(JSON.stringify(data, undefined, 4));
+//     return prisma.query.users(null, "{id name posts { id title}}");
+//   })
+//   .then(data => console.log(JSON.stringify(data, undefined, 4)));
+
+// prisma.mutation
+//   .updatePost({
+//     where: { id: "cjnsu5ep6004q0805bn1tesz1" },
+//     data: { published: false }
+//   })
+//   .then(data => {
+//     console.log(data);
+//     return prisma.query.users(null, "{id name posts { id title published }}");
+//   })
+//   .then(data => console.log(JSON.stringify(data, undefined, 4)));

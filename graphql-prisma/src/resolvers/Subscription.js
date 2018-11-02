6b815cabbb2678d6d75819e048eaf38772135302
siddhant1 <sidhant.manchanda@gmail.com>
2018-11-02 22:36:37 +0530

    Added Prisma Subscription

--- graphql-prisma/src/resolvers/Subscription.js
+++ graphql-prisma/src/resolvers/Subscription.js
@@ -1,28 +1,49 @@
 const Subscription = {
-  count: {
-    subscribe(parent, args, { pubsub }, info) {
-      //Step1 : Set up a subscription
-      let count = 0;
-      setInterval(() => {
-        count++;
-        //Step2 Publish to subscription
-        pubsub.publish("count", { count });
-      }, 1000);
-      return pubsub.asyncIterator("count");
-    }
-  },
+  // count: {
+  //   subscribe(parent, args, { pubsub }, info) {
+  //     //Step1 : Set up a subscription
+  //     let count = 0;
+  //     setInterval(() => {
+  //       count++;
+  //       //Step2 Publish to subscription
+  //       pubsub.publish("count", { count });
+  //     }, 1000);
+  //     return pubsub.asyncIterator("count");
+  //   }
+  // },
   comment: {
-    subscribe(parent, args, { pubsub, db }, info) {
-      let post = db.posts.find(post => post.id === args.postId);
-      if (!post) {
-        throw new Error("No Post Found");
-      }
-      return pubsub.asyncIterator(`comment ${post.id}`);
+    subscribe(parent, { postId }, { prisma }, info) {
+      return prisma.subscription.comment(
+        {
+          where: {
+            node: {
+              post: {
+                id: postId
+              }
+            }
+          }
+        },
+        info
+      );
+      // let post = db.posts.find(post => post.id === args.postId);
+      // if (!post) {
+      //   throw new Error("No Post Found");
+      // }
+      // return pubsub.asyncIterator(`comment ${post.id}`);
     }
   },
   post: {
-    subscribe(parent, args, { pubsub, db }, info) {
-      return pubsub.asyncIterator("post");
+    subscribe(parent, args, { db, prisma }, info) {
+      return prisma.subscription.post(
+        {
+          where: {
+            node: {
+              published: true
+            }
+          }
+        },
+        info
+      );
     }
   }
 };
